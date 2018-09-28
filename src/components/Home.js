d7c5abdbe4799bb9634add8218ed37ed4241ec9a
Kevin Ahn <kevinahn7@hotmail.com>
2018-09-28 11:29:02 -0700

    Change Home component from class to a function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Home extends React.Component {
+const Home = () => {
+	
+	return (
+		<div>
+			<Link to="/watch">Watch</Link>
+		</div>
+	);
 
-
-	render() {
-		return (
-			<div>
-				<Link to="/watch">Watch</Link>
-			</div>
-		);
-	}
 }
 
 export default Home;
