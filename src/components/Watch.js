5278f6591c87520e4610a26d7e9311ff68fca815
Kevin Ahn <kevinahn7@hotmail.com>
2018-10-01 09:33:04 -0700

    Get the video player working

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -2,11 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Watch = ({currentVideo}) => {
-
 	return (
 		<div>
-			{/* {console.log(currentVideo)} */}
-			<iframe width="480" height="270" src="//www.youtube.com/embed/cQ_i5tYJ2_k" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+			{ console.log(currentVideo.player.embedHtml) }
+			<iframe width="480" height="270" src={"\/\/www.youtube.com/embed/" + currentVideo.id} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
 			{/* Add the video id to the url */}
 			{/* If I have this, i dont think i will need to import player */}
 		</div>
