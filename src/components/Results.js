996741f460781d56c90464492e06483173b4ad08
Kevin Ahn <kevinahn7@hotmail.com>
2018-10-01 23:25:11 -0700

    Add type to add more to search results in state, but unsuccessfully add a listener to listen to activate that reducer and action by scrolling

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,70 +2,101 @@ import React from 'react';
 import VideoThumbnail from './VideoThumbnail';
 import ChannelThumbnail from './ChannelThumbnail';
 import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroller';
+import { fetchMoreSearchResults } from './../actions';
+import { connect } from 'react-redux';
 
-const resultsStyle = {
-	maxWidth: "1328px",
-	boxSizing: "border-box",
-	width: "100%",
-	margin: "0 auto",
-	padding: "16px 24px"
-}
+class Results extends React.Component {
+	constructor(props) {
+		super(props)
+		this.handleScroll = this.handleScroll.bind(this);
+	}
 
-const filterContainerStyle = {
-	boxShadow: "0 1px #eee",
-	height: "44px",
-	display: "flex",
-	alignItems: "center"
-}
+	loadMore = (searchQuery, pageToken) => {
+		this.props.dispatch(fetchMoreSearchResults(searchQuery, pageToken))
+	}
 
-const filterStyle = {
-	margin: "0",
-	padding: "10px 16px",
-	fontSize: ".9rem",
-	color: "black"
-}
+	handleScroll = (searchQuery, pageToken) => {
+		let distanceFromBottom = document.body.scrollHeight - window.innerHeight - window.scrollY;
+		console.log(distanceFromBottom)
+		if (distanceFromBottom < 300) this.loadMore(searchQuery, pageToken)
+	}
 
-const resultItemsStyle = {
-	display: "grid",
-	gridTemplateColumns: "1fr",
-	gridGap: "15px"
-}
+	resultsStyle = {
+		maxWidth: "1328px",
+		boxSizing: "border-box",
+		width: "100%",
+		margin: "0 auto",
+		padding: "16px 24px"
+	}
+	
+	filterContainerStyle = {
+		boxShadow: "0 1px #eee",
+		height: "44px",
+		display: "flex",
+		alignItems: "center"
+	}
+	
+	filterStyle = {
+		margin: "0",
+		padding: "10px 16px",
+		fontSize: ".9rem",
+		color: "black"
+	}
+	
+	resultItemsStyle = {
+		display: "grid",
+		gridTemplateColumns: "1fr",
+		gridGap: "15px"
+	}
 
-const Results = ({searchResults}) => {
-
-  	return (
-		<div style={resultsStyle}>
-			<div style={filterContainerStyle}>
-				<p style={filterStyle}>FILTER</p>
+	render() {
+		let currentSearch = this.props.currentSearch
+		let searchResults = currentSearch.searchResults;
+		return (
+			<div style={this.resultsStyle} onClick={() => this.handleScroll(currentSearch.searchQuery, currentSearch.pageToken)} ref={(scroller) => {this.scroller = scroller;}}>
+				<div style={this.filterContainerStyle}>
+					<p style={this.filterStyle}>FILTER</p>
+				</div>
+				<div style={this.resultItemsStyle}>
+					{/* <InfiniteScroll
+						pageStart={0}
+						loadMore={loadMore(currentSearch.searchQuery, currentSearch.pageToken)}
+						hasMore={true}
+						threshold={4000}
+						loader={<div>Loading....</div>}
+					> */}
+						{Object.keys(searchResults).map(function(index) {
+							if (searchResults[index].id.kind === "youtube#video") {
+								return <VideoThumbnail
+									key={index}
+									videoTitle={searchResults[index].snippet.title}
+									videoDescription={searchResults[index].snippet.description}
+									videoId={searchResults[index].id.videoId}
+									channelId={searchResults[index].snippet.channelId}
+									channelTitle={searchResults[index].snippet.channelTitle}
+									image={searchResults[index].snippet.thumbnails.medium.url}
+									publishedAt={searchResults[index].snippet.publishedAt} />
+							} else if (searchResults[index].id.kind === "youtube#channel"){
+								return <ChannelThumbnail
+									key={index}
+									channelTitle={searchResults[index].snippet.title}
+									channelDescription={searchResults[index].snippet.description}
+									channelId={searchResults[index].snippet.channelId}
+									image={searchResults[index].snippet.thumbnails.high.url} />
+							} //Add youtube playlist conditional
+						})}
+					{/* </InfiniteScroll> */}
+	
+				</div>
 			</div>
-			<div style={resultItemsStyle}>
-				{Object.keys(searchResults).map(function(index) {
-					if (searchResults[index].id.kind === "youtube#video") {
-						return <VideoThumbnail
-							key={index}
-							videoTitle={searchResults[index].snippet.title}
-							videoDescription={searchResults[index].snippet.description}
-							videoId={searchResults[index].id.videoId}
-							channelId={searchResults[index].snippet.channelId}
-							channelTitle={searchResults[index].snippet.channelTitle}
-							image={searchResults[index].snippet.thumbnails.medium.url}
-							publishedAt={searchResults[index].snippet.publishedAt} />
-					} else if (searchResults[index].id.kind === "youtube#channel"){
-						return <ChannelThumbnail
-							key={index}
-							channelTitle={searchResults[index].snippet.title}
-							channelDescription={searchResults[index].snippet.description}
-							channelId={searchResults[index].snippet.channelId}
-							image={searchResults[index].snippet.thumbnails.high.url} />
-					} //Add youtube playlist conditional
-				})}
-			</div>
-		</div>
-	);
+		);
+	}
+
 }
 
 Results.propTypes = {
- 	searchResults: PropTypes.array
+	currentSearch: PropTypes.object
 };
 
-export default Results;
+export default connect()(Results);
