77f5259dd62a3457c4c03cbba982844ccb0c3940
Kevin Ahn <kevinahn7@hotmail.com>
2018-11-12 11:52:54 -0800

    Add the main categories for the SideList component

--- src/components/SideList.js
+++ src/components/SideList.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import Menu from "@material-ui/icons/Menu";
+import Home from "@material-ui/icons/Home";
+import Whatshot from "@material-ui/icons/Whatshot";
+import History from "@material-ui/icons/History";
 import { Link } from 'react-router-dom';
 import youtubeLogo from '../assets/youtube-logo.png';
 
@@ -40,6 +43,33 @@ const SideList = () => {
         paddingRight: "51px"
     }
 
+    const mainCategoriesContainerStyle = {
+        padding: "12px 0",
+        borderBottom: "hsla(0, 0%, 93%, 1) 1px solid"
+    }
+
+    const mainCategoriesStyle = {
+        padding: "0 24px",
+        height: "40px",
+        display: "flex",
+        alignItems: "center",
+        fontSize: "0.87rem"
+    }
+
+    const mainCategoryHomeIconStyle = {
+        marginRight: "24px",
+        height: "24px",
+        width: "24px",
+        opacity: "0.5"
+    }
+
+    const mainCategoryOtherIconStyle = {
+        marginRight: "26px",
+        height: "22px",
+        width: "22px",
+        opacity: "0.5"
+    }
+
 	return (
         <div style={sideListStyle}>
             <div style={headerStyle}>
@@ -48,7 +78,17 @@ const SideList = () => {
                 </IconButton>
                 <Link to="/"><img style={youTubeLogoStyle} src={youtubeLogo} alt="The YoutTube logo"/></Link>
             </div>
-            
+            <div style={mainCategoriesContainerStyle}>
+                <div style={mainCategoriesStyle}>
+                    <Home style={mainCategoryHomeIconStyle}/> Home
+                </div>
+                <div style={mainCategoriesStyle}>
+                    <Whatshot style={mainCategoryOtherIconStyle} /> Trending
+                </div>
+                <div style={mainCategoriesStyle}>
+                    <History style={mainCategoryOtherIconStyle}/> History
+                </div>
+            </div>
         </div>
 	);
 }
